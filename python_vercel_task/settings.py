c7ccd15999d354e6e12b7935e6842b586849fb5a
fernando <fleitasbaez55@gmail.com>
2024-11-19 11:36:02 -0300

    Cambios en los archivos

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'python_vercel_task.wsgi.application'
+WSGI_APPLICATION = 'python_vercel_task.wsgi.app'
 
 
 # Database
